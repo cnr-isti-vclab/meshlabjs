f03dcf35ea5ed3b1ab2cc31226e976a33d28dd5b
Paolo Cignoni <cignoni@isti.cnr.it>
2015-03-17 22:19:04 +0100

    splitting timings in loadprocrender1

--- test/LoadProcRender1/build.sh
+++ test/LoadProcRender1/build.sh
@@ -1,7 +1,4 @@
 # Remember to set up the environment before invoking this build. 
 # source emsdk_set_env.sh 
 
-#my local path
-em++ --bind -I /Users/maurizio/Desktop/Universita/F3D/vcg-code/vcglib/ open.cpp -s EXPORTED_FUNCTIONS="['_allocator','_openMesh','_getVertexNumber','_getVertexVector','_getFaceNumber','_getFaceVector']" -o open.js
-#em++ -I ../../../vcglib/ refine.cpp -s EXPORTED_FUNCTIONS="['_allocator','_refine']" -o refine.js
-# em++ refine.cpp --bind -o refine.js
+em++ --bind -I ../../../vcglib/ open.cpp -s EXPORTED_FUNCTIONS="['_allocator','_openMesh','_getVertexNumber','_getVertexVector','_getFaceNumber','_getFaceVector']" -o open.js
