d7849876ebb3c550f6a35c371b69d2628eb9f421
m3c4 <maurizio.idini@gmail.com>
2015-04-01 00:12:39 +0200

    fix LoadProcRender2.0

---
+++ test/LoadProcRender2.0/module.js
@@ -0,0 +1,25 @@
+        /*
+Module is a global JavaScript object with attributes that 
+Emscripten-generated code calls at various points in its execution.
+
+Developers can provide an implementation of Module to control 
+the execution of code. For example, to define how notification 
+messages from Emscripten are displayed, developers implement the 
+Module.print attribute.
+*/
+        var Module = {
+            preRun: [],
+            postRun: [],
+            print: (function () {
+                var element = document.getElementById('output');
+                if (element) element.value = ''; // clear browser cache
+                return function (text) {
+                    text = Array.prototype.slice.call(arguments).join(' ');
+                    console.log(text);
+                    if (element) {
+                        element.value += text + "\n";
+                        element.scrollTop = element.scrollHeight; // focus on bottom
+                    }
+                };
+            })()
+        };
