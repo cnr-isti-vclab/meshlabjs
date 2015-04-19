dfadccdeef5be8d0fd2d8c48e1f9b14d01d0ab08
m3c4 <maurizio.idini@gmail.com>
2015-04-19 13:58:50 +0200

    test

---
+++ js/engine/Engine.js
@@ -0,0 +1,208 @@
+        var camera, scene, renderer, controls, time, mesh, ptrMesh;
+        //declaration of plugin global variable
+        var Refine,Smooth;
+
+        //array of opened mesh
+        var openedMesh=[];
+        var countRadio=0;
+
+        //Declaration DAT.GUI
+        var masterGui = function() {
+            this.OpenMesh = function() {
+                $('#files').click();
+                }; //end OpenMesh di dat.gui
+                this.step = 1;
+                this.refine = function() { 
+                    console.time("Refine time ");
+                    Refine = new Module.MyRefine(ptrMesh);
+                    Refine.myRefine(step);
+                    console.timeEnd("Refine time ");
+                    console.time("Update mesh ");
+                    createMesh(ptrMesh);
+                    console.timeEnd("Update mesh ");
+                }; //end refine
+            };//end mastergui
+        var master = new masterGui();
+        var gui = new dat.GUI({ autoPlace: false });
+        document.getElementById('controls').appendChild(gui.domElement);
+        gui.add(master, 'OpenMesh');
+        var stepController = gui.add(master, 'step',0,5).step(1);
+        var step = 1;
+        stepController.onChange(function(value) {
+            step=value;
+        });
+
+        gui.add(master, 'refine');
+
+
+
+            //handler for plugin REFINE
+            // document.getElementById('refinement').addEventListener('click', function refineMesh(){
+                
+            // });
+
+        init();
+
+
+        function handleFileSelect(evt) {
+            var files = evt.target.files; // FileList object
+                console.log("Name: ", files[0].name);
+                console.log("Size: ", files[0].size);
+                console.time("File Reading Time");
+
+                //extract format file
+                var fileName = files[0].name;
+                var format = fileName.split(".");
+                format = format[format.length-1];
+                fileName= "tmp." + format;
+                switch(format){
+                    case "off": {break;}
+                    case "obj": {break;}
+                    case "ply": {break;}
+                    case "stl": {break;}
+                    case "vmi": {break;}
+                    default : {
+                        alert("MeshLabJs allows file format '.off', '.ply', '.vmi', '.obj' and '.stl'. \nTry again.")
+                        return;
+                    }
+                }
+            
+                var fileToLoad = files[0];
+                var fileReader = new FileReader();
+            
+                fileReader.onload = function (fileLoadedEvent) {
+
+            //  Emscripten need a Arrayview so from the returned arraybuffer we must create a view of it as 8bit chars
+                var int8buf = new Int8Array(fileLoadedEvent.target.result);
+                FS.createDataFile("/", fileName, int8buf, true, true);
+
+                console.log("Read file", fileLoadedEvent.target.result.byteLength );
+                console.timeEnd("File Reading Time");
+
+                console.time("Parsing mesh Time");
+                var Opener = new Module.Opener();
+                var resOpen = Opener.openMesh(fileName);
+                if(resOpen!=0){
+                    alert("Ops! Error in Opening File.\nTry again.");
+                    FS.unlink(fileName);
+                }
+                else {
+                console.log("Open mesh with result "+resOpen);
+                console.timeEnd("Parsing mesh Time");
+                ptrMesh = Opener.getMesh();
+                createMesh(ptrMesh);
+                animate();
+
+                FS.unlink(fileName);
+                
+                // go
+                // if(openedMesh.length>4){
+                //     document.getElementById('field').elements[0].remove();
+                //     document.getElementsByTagName('label')[0].parentNode.removeChild(document.getElementsByTagName('label')[0]);
+                //     document.getElementsByTagName('br')[0].parentNode.removeChild(document.getElementsByTagName('br')[0]);
+                //     // openedMesh.shift();
+                // }
+                //go
+
+                //count how many radiobox exist
+                // var countRadio=0;
+                // var field = document.getElementById('field')
+                // for(var i=0; i<field.elements.length; i++) {
+                //     var el = field.elements[i];
+                //     if(el.type == 'radio') 
+                //         countRadio++;
+                // }
+                //create new checkbox and relative label, append this
+                // var checkbox = document.createElement('input');
+                // checkbox.type = "checkbox";
+                // checkbox.name = "checkbox";
+                // checkbox.value = ptrMesh;
+                // checkbox.text = files[0].name;
+                // checkbox.checked="checked";
+
+                //go
+                // var radio = document.createElement('input');
+                // radio.type = "radio";
+                // radio.name = "mesh";
+                // radio.value = countRadio;
+                // radio.text = files[0].name;
+                // radio.checked = "checked";
+                // $(radio).on('click',  OnChangeRadio);
+                //go
+                //     // var int8buf = new Int8Array(openedMesh[radio.value]); 
+                //     // FS.createDataFile("/", fileName, int8buf, true, true);
+                //     // var Opener = new Module.Opener();
+                //     // var resOpen = Opener.openMesh(fileName);
+                
+                //     // console.log("Open mesh with result "+resOpen);
+                //     // console.timeEnd("Parsing mesh Time");
+                //     // ptrMesh = Opener.getMesh();
+                //     // createMesh(ptrMesh);
+                //     // animate();
+                //     // FS.unlink(fileName);
+                    
+                //     alert(radio.value);
+                //  });
+                // radio.onchange = OnChangeRadio(this);
+                // radio.addEventListener ("RadioStateChange", alert("pippo"), false);
+                
+                //go
+                // var label = document.createElement('label')
+                // label.htmlFor = files[0].name;
+                // label.appendChild(document.createTextNode(files[0].name));
+                // // document.getElementById('field').appendChild(checkbox);
+                // document.getElementById('field').appendChild(radio);
+                // document.getElementById('field').appendChild(label);
+                // var br = document.createElement('br');
+                // document.getElementById('field').appendChild(br);
+                
+                // openedMesh[countRadio]=int8buf;
+                // countRadio++;
+                //go
+
+                }//end else
+                }; //end Onload
+
+                fileReader.readAsArrayBuffer(fileToLoad, "UTF-8");  // Efficient binary read.
+            }
+            document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+            // $('input[type="radio"]').on('click change', function(e) {
+            // alert(e.type);
+            // });
+        function OnChangeRadio(){              
+        // $('input[type="radio"]').on('click', function(e) {
+            var myRadio = $('input[name=mesh]');
+            var checkedValue = myRadio.filter(':checked').val();
+            var int8buf = new Int8Array(openedMesh[checkedValue]); 
+            var fileName = $("input:radio:checked").next().text();
+            FS.createDataFile("/", fileName, int8buf, true, true);
+            var Opener = new Module.Opener();
+            var resOpen = Opener.openMesh(fileName);    
+            console.log("Open mesh with result "+resOpen);
+            console.timeEnd("Parsing mesh Time");
+            ptrMesh = Opener.getMesh();
+            createMesh(ptrMesh);
+            animate();
+            FS.unlink(fileName);
+        // });
+        }
+
+
+
+            
+
+
+
+            //handler for plugin SMOOTH
+            // document.getElementById('smoothed').addEventListener('click', function smoothMesh(){
+            // console.time("Smooth time ");
+            // Smooth = new Module.MySmooth(ptrMesh);
+            // Smooth.mySmooth(1);
+            // console.timeEnd("Smooth time ");
+            // console.time("Update mesh ");
+            // createMesh(ptrMesh);
+            // console.timeEnd("Update mesh ");
+            // });
+            
+
---
+++ js/engine/Rendering.js
@@ -0,0 +1,85 @@
+        function init() {
+        var div_WIDTH = document.getElementById("renderingMesh").offsetWidth, 
+            div_HEIGHT = document.getElementById("renderingMesh").offsetHeight;
+        // sezione di set-up di progetto, di iniziazione
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(  45, div_WIDTH/div_HEIGHT, 0.1, 1000  );
+        camera.position.z = 15;
+        renderer = new THREE.WebGLRenderer();
+        renderer.shadowMapEnabled = true;
+        renderer.setClearColor(0x000000, 1); 
+        renderer.setSize(div_WIDTH, div_HEIGHT);
+        var container = document.getElementById("renderingMesh");
+        container.appendChild(renderer.domElement);
+        scene.add( camera );
+        controls = new THREE.TrackballControls( camera ,container);
+        controls.rotateSpeed = 4.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 2.0;
+        controls.noZoom = false;
+        controls.noPan = false;
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+        controls.keys = [ 65, 83, 68 ];
+        controls.addEventListener( 'change', render );
+        
+        
+        } //end init
+        
+        function animate() {        
+            requestAnimationFrame( animate );//crea un loop
+             controls.update();
+             render();
+        }
+         
+        function render() {
+            renderer.render(scene, camera);
+        }
+
+        function createMesh(ptrMesh) {
+
+            console.time("Getting MeshProperties Time");
+            var MeshProperties = new Module.MeshLabJs(ptrMesh);
+            var VN = MeshProperties.getVertexNumber();
+            var vert = MeshProperties.getVertexVector();
+            var face = MeshProperties.getFaceVector();
+            var FN = MeshProperties.getFaceNumber();
+            console.timeEnd("Getting MeshProperties Time");
+            
+            scene.remove(mesh);
+            var geometry = new THREE.Geometry();
+            console.log("Vertices are "+VN);
+            console.log("Faces are "+FN);
+            console.time("Time to create: ");
+            for(var i=0; i<VN*3; i++){
+                var v1 = Module.getValue(vert+parseInt(i*4),'float'); i++;
+                var v2 = Module.getValue(vert+parseInt(i*4),'float'); i++;
+                var v3 = Module.getValue(vert+parseInt(i*4),'float'); 
+                geometry.vertices.push( new THREE.Vector3(v1,v2,v3) );
+            }
+            for(var i=0; i<FN*3; i++){
+                var a = Module.getValue(face+parseInt(i*4),'*'); i++;
+                var b = Module.getValue(face+parseInt(i*4),'*'); i++;
+                var c = Module.getValue(face+parseInt(i*4),'*'); 
+                geometry.faces.push( new THREE.Face3(a,b,c));
+            }
+            console.log("geometry created.");
+            console.timeEnd("Time to create: ");
+            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true }); 
+            mesh = new THREE.Mesh( geometry, material );
+            box = new THREE.Box3().setFromObject(mesh);
+            scale = 7.0/box.min.distanceTo(box.max);
+            mesh.position = box.center().negate().multiplyScalar(scale);             
+            mesh.scale = new THREE.Vector3(scale,scale,scale);
+            mesh.updateMatrix();
+            mesh.matrixAutoUpdate = false;
+            scene.add(mesh);
+        }
+
+        // function addMesh(name){
+        //     scene.add( name );
+        // }
+
+        // function removeMesh(name) {
+        //     scene.remove( name );
+        // }
