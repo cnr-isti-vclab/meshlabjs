ac7ec91fa1135a1fdce101d96fa1f324312d885f
Paolo Cignoni <cignoni@isti.cnr.it>
2015-06-08 15:13:42 +0200

    filled some info in the about dialog box

--- test/jQueryGUI/v3/js/mlj/gui/Logo.js
+++ test/jQueryGUI/v3/js/mlj/gui/Logo.js
@@ -7,7 +7,8 @@
 MLJ.gui.Logo = {};
 
 (function (gui) {
-    var LOGO_WIDTH = 86;
+    var LOGO_WIDTH = 96;
+    var LOGO_HEIGHT = 821*LOGO_WIDTH/1023;
     var insets = 10;
     var _PiP;
     var _$dialog;
@@ -18,7 +19,23 @@ MLJ.gui.Logo = {};
 
                 var $logo = $('<img id="logo" src="../../../img/vcglogo200609_1024px.png">');
 
-                _$dialog = $('<div id="dialog" title="Dialog Title">About</div>').hide();
+                _$dialog = $('  <div id="dialog" title="About MeshLabJS">\
+                    Copyright(C) 2015<br>\
+                    <br>\
+                    <a href="http://vcg.isti.cnr.it/%7Ecignoni">Paolo Cignoni</a> <br>\
+                    <a href="http://vcg.isti.cnr.it"> Visual Computing Lab</a> <br>\
+                    <a href="http://www.isti.cnr.it"> ISTI - Italian National Research Council</a><br><br> \
+<b>MeshLabJS</b> is a web-based mesh processing system for \
+cleaning, filtering, editing and rendering of unstructured 3D triangular meshes. \
+The system is heavily inspired on desktop version of <a href="http://meshlab.sf.net">MeshLab</a>.<br> \
+MeshLabJS for the mesh processing tasks relies on the \
+<a href="http://vcg.sf.net">VCG</a> C++ library compiled to asm.js using emscripten \
+<br>\
+<br><b>Authors</b> (in first commit order)<br>\
+Paolo Cignoni (Project Leader) <br> \
+Maurizio Idini<br>\
+Stefano Gabriele <br>\
+</div>').hide();
                 $('body').append(_$dialog);
 
                 $logo.load(function () {
@@ -30,7 +47,7 @@ MLJ.gui.Logo = {};
 
                 $(document).ready(function () {
                     var x = $('#tools-pane').outerWidth() + insets;
-                    var y = $(window).height() - LOGO_WIDTH;
+                    var y = $(window).height() - LOGO_HEIGHT - insets;
                     _PiP.setX(x);
                     _PiP.setY(y);
                 });
@@ -38,7 +55,7 @@ MLJ.gui.Logo = {};
                 $(window).resize(function () {
                     var $tp = $('#tools-pane');
                     var newX = $tp.outerWidth() + $tp.offset().left + insets;
-                    var newY = $(window).height() - LOGO_WIDTH;
+                    var newY = $(window).height() - LOGO_HEIGHT - insets;
                     _PiP.setX(newX);
                     _PiP.setY(newY);
                 });
