0b562903fa699bb7a3d92064f451b281b1c224a5
Stefano Gabriele <stefano_gabriele@yahoo.it>
2015-06-16 18:37:13 +0200

    Filters code refactoring

--- test/jQueryGUI/v4/js/mlj/plugins/filters/Random.js
+++ test/jQueryGUI/v4/js/mlj/plugins/filters/Random.js
@@ -1,54 +1,30 @@
 
 (function (plugin, gui, scene) {
 
-    var filter = new plugin.Filter("Random Displacement");
+    var filter = new plugin.Filter("Random Displacement", false);
     var DEFAULT_DISPLACEMENT = 0.01;
     var _displacement = DEFAULT_DISPLACEMENT;
 
-    function _random(meshFile, displacement) {
-        console.time("Random time");
-        Module.RandomDisplacement(meshFile.ptrMesh, displacement);
-        console.timeEnd("Random time");
-        console.time("Update mesh ");
-        scene.updateLayer(meshFile);
-        console.timeEnd("Update mesh ");
-    }
-
-    function _randomSelected() {
-        var meshFile = scene.getSelectedLayer();
-        _random(meshFile, _displacement);
-    }
-
-    function _randomAll() {
-        var ptr = scene.getLayers().pointer();
-        while (ptr.hasNext()) {
-            _random(ptr.next(), _displacement);
-        }
-    }
-
-    filter._main = function (accordEntry) {
-
-        var apply = gui.build.button.Button("Apply", "Apply to selected mesh");
-        var applyAll = gui.build.button.Button("Apply all", "Apply to all mesh in the scene");
-        accordEntry.addHeaderButton(apply, applyAll);
+    filter._init = function (accordEntry) {
 
         var spinner = gui.build.Spinner({max: 0.1, min: 0.01, step: 0.01, defval: DEFAULT_DISPLACEMENT});
 
         accordEntry.appendContent(
                 gui.component.Grid(gui.build.Label("Displacement"), spinner));
 
-        apply.onClick(function () {
-            _randomSelected();
-        });
-
-        applyAll.onClick(function () {
-            _randomAll();
-        });
-
         spinner.onChange(function (event) {
             _displacement = parseFloat(event.target.value);
         });
 
+    };
+
+    filter._applyTo = function (meshFile) {
+        console.time("Random time");
+        Module.RandomDisplacement(meshFile.ptrMesh, _displacement);
+        console.timeEnd("Random time");
+        console.time("Update mesh ");
+        scene.updateLayer(meshFile);
+        console.timeEnd("Update mesh ");
     };
 
     plugin.install(filter);
--- test/jQueryGUI/v4/js/mlj/plugins/filters/Refine.js
+++ test/jQueryGUI/v4/js/mlj/plugins/filters/Refine.js
@@ -1,55 +1,30 @@
 
 (function (plugin, gui, scene) {
 
-    var filter = new plugin.Filter("Refine");
+    var filter = new plugin.Filter("Refine",false);
     var DEFAULT_STEP = 1;
     var _step = DEFAULT_STEP;
 
-    function _refine(meshFile, step) {
-        console.time("Refine time");
-        var refine = new Module.MyRefine(meshFile.ptrMesh);
-        refine.myRefine(step);
-        console.timeEnd("Refine time");
-        console.time("Update mesh");
-        scene.updateLayer(meshFile);
-        console.timeEnd("Update mesh");
-    }
-
-    function _refineSelected() {
-        var meshFile = scene.getSelectedLayer();
-        _refine(meshFile, _step);
-    }
-
-    function _refineAll() {
-        var ptr = scene.getLayers().pointer();
-        while (ptr.hasNext()) {
-            _refine(ptr.next(), _step);
-        }
-    }
-
-    filter._main = function (accordEntry) {
-
-        var apply = gui.build.button.Button("Apply", "Apply to selected mesh");
-        var applyAll = gui.build.button.Button("Apply all", "Apply to all mesh in the scene");
-        accordEntry.addHeaderButton(apply, applyAll);
-
+    filter._init = function (accordEntry) {
         var spinner = gui.build.Spinner({max: 5, min: 1, defval: DEFAULT_STEP});
 
         accordEntry.appendContent(
                 gui.component.Grid(gui.build.Label("Step"), spinner));
 
-        apply.onClick(function () {
-            _refineSelected();
-        });
-
-        applyAll.onClick(function () {
-            _refineAll();
-        });
-
         spinner.onChange(function (event) {
             _step = parseInt(event.target.value);
         });
+    };
 
+
+    filter._applyTo = function (meshFile) {
+        console.time("Refine time");
+        var refine = new Module.MyRefine(meshFile.ptrMesh);
+        refine.myRefine(_step);
+        console.timeEnd("Refine time");
+        console.time("Update mesh");
+        scene.updateLayer(meshFile);
+        console.timeEnd("Update mesh");
     };
 
     plugin.install(filter);
