8f05ea0342facaea6f0b0181f4d5d72a9a4977a8
Stefano Gabriele <stefano_gabriele@yahoo.it>
2015-07-07 02:19:39 +0200

    Albedo color

--- js/mlj/plugins/rendering/ColorWheel.js
+++ js/mlj/plugins/rendering/ColorWheel.js
@@ -1,12 +1,36 @@
 
 (function (plugin, core, scene) {
-    
-     var plug = new plugin.Rendering({
-        name: "ColorWheel",        
+
+    var plug = new plugin.Rendering({
+        name: "ColorWheel",
         tooltip: "ColorWheel Tooltip",
-        icon: "img/icons/color.png"        
+        icon: "img/icons/color.png",
+        parameters: {
+            color: new THREE.Color('#a0a0a0')
+        }
     });
-    
+
+
+    var albedoColor;
+
+    plug._init = function (guiBuilder) {
+
+        albedoColor = guiBuilder.Color({
+            label: "Albedo color",
+            tooltip: "Diffuse color of the material",
+            color: '#' + plug.parameters.color.getHexString(),
+            onChange: function (hex) {
+                var meshFile = scene.getSelectedLayer();
+                meshFile.material.setColor('#' + hex);
+            }
+        });
+
+    };
+
+    plug._update = function () {
+        var meshFile = scene.getSelectedLayer();
+        albedoColor.setColor(meshFile.material.parameters.color.getHexString());
+    };
 
     plugin.install(plug);
 
