f304219f05c0dfbcf336f31f6131808be9e69afc
Paolo Cignoni <cignoni@isti.cnr.it>
2015-07-13 11:47:52 +0200

    Still hand syncing

--- sources/build.sh
+++ sources/build.sh
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-emcc --bind -s DEMANGLE_SUPPORT=1 -I ../../vcglib/ MeshLabJs.cpp Opener.cpp  Refine.cpp Smooth.cpp Saver.cpp Random.cpp ../../vcglib/wrap/ply/plylib.cpp  -s TOTAL_MEMORY=268435456 -O3 -o ../js/generated/MeshlabJs.js
-=======
 emcc --bind -s DEMANGLE_SUPPORT=1 -I ../../vcglib/ MeshLabJs.cpp CppMesh.cpp Meshing.cpp Sampling.cpp Create.cpp Refine.cpp Smooth.cpp Saver.cpp Random.cpp ../../vcglib/wrap/ply/plylib.cpp  -s TOTAL_MEMORY=268435456 -O3 -o ../js/generated/MeshlabJs.js
->>>>>>> master
-=======
-emcc --bind -s DEMANGLE_SUPPORT=1 -I ../../vcglib/ MeshLabJs.cpp CppMesh.cpp Meshing.cpp Sampling.cpp Create.cpp Refine.cpp Smooth.cpp Saver.cpp Random.cpp ../../vcglib/wrap/ply/plylib.cpp  -s TOTAL_MEMORY=268435456 -O3 -o ../js/generated/MeshlabJs.js
->>>>>>> master
 
 #with split
 # EMCC_FAST_COMPILER=0 emcc --bind --split 1024000 -s DEMANGLE_SUPPORT=1 -I ../../../vcglib/ open.cpp ../../../vcglib/wrap/ply/plylib.cpp -s TOTAL_MEMORY=268435456  -o open.js
