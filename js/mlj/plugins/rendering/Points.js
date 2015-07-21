3343e05a1dfdfc6d84801776fb08db2558b7aa48
Stefano Gabriele <stefano_gabriele@yahoo.it>
2015-07-21 20:02:32 +0200

    Points rendering feature uses sprites

--- js/mlj/plugins/rendering/Points.js
+++ js/mlj/plugins/rendering/Points.js
@@ -3,25 +3,20 @@
 
     var DEFAULTS = {
         color: new THREE.Color('#0277BD'),
-        size: 2
+        size: 1,
+        //PRELOADING NEEDED ...
+        texture: THREE.ImageUtils.loadTexture("js/mlj/plugins/rendering/textures/sprites/disc.png")
     };
 
     //Shader
     var POINTS = {
-        uniforms: THREE.UniformsUtils.merge([
-            {
-                "size": {type: "f", value: DEFAULTS.size},
-                "color": {type: "c", value: DEFAULTS.color}
-            }
-        ]),
+
         vertexShader: [
             "uniform float size;",
-            "uniform vec3 color;",
-            "varying vec3 vColor;",
+            "attribute float minSize;",
             "void main() {",
-            "   vColor = color;",
             "   vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
-            "   gl_PointSize = size;",
+            "   gl_PointSize = minSize + size;",
             "   gl_Position = projectionMatrix * mvPosition;",
             "   gl_Position.z -= (size / 1000.0);",
             "}"
@@ -29,9 +24,11 @@
         ].join("\n"),
         fragmentShader: [
             "uniform vec3 color;",
-            "varying vec3 vColor;",
+            "uniform sampler2D texture;",
             "void main() {",
-            "   gl_FragColor = vec4( color * vColor, 1.0 );",
+            "   gl_FragColor = vec4(color, 1.0 );",
+            "   gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );",
+            "   if ( gl_FragColor.a < ALPHATEST ) discard;",
             "}"
 
         ].join("\n")
@@ -67,28 +64,40 @@
 
     plug._applyTo = function (meshFile, on) {
 
-        if (on == false) {
+        if (on === false) {
             scene.removeOverlayLayer(meshFile, plug.getName());
             return;
         }
+
         var geom = meshFile.getThreeMesh().geometry.clone();
-        var uniforms = THREE.UniformsUtils.clone(POINTS.uniforms);
+                
         var params = meshFile.overlaysParams.getByKey(plug.getName());
-
-        uniforms.color.value = params.color;
-        uniforms.size.value = params.size;
-
-        var parameters = {
-            fragmentShader: POINTS.fragmentShader,
-            vertexShader: POINTS.vertexShader,
-            uniforms: uniforms
+        var attributes = {
+            minSize: {type: 'f', value: []}
         };
+                        
+        var uniforms = {
+            color: {type: "c", value: params.color},
+            size: {type: "f", value: params.size},
+            texture: {type: "t", value: DEFAULTS.texture}
+        };
+                        
+        var shaderMaterial = new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            attributes: attributes,
+            vertexShader: POINTS.vertexShader,
+            fragmentShader: POINTS.fragmentShader,
+            alphaTest: 0.9
+        });
+                
+        var points = new THREE.PointCloud(geom, shaderMaterial);
+        var values_minSize = attributes.minSize.value;
+        
+        for (var v = 0, vl = geom.vertices.length; v < vl; v++) {
+            values_minSize[ v ] = 10;
+        }
 
-        var mat = new THREE.ShaderMaterial(parameters);
-        var points = new THREE.PointCloud(geom, mat);
         scene.addOverlayLayer(meshFile, plug.getName(), points);
-
-
     };
 
     plugin.Manager.install(plug);
