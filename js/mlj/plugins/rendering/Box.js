ede6fb5f81af20dae21e02878bf05ab2fa9076b9
sgiammori <stefano.giammori@gmail.com>
2015-07-22 18:50:37 +0200

    Added unquoted BBox to rendering plugins.

--- js/mlj/plugins/rendering/Box.js
+++ js/mlj/plugins/rendering/Box.js
@@ -9,6 +9,22 @@
         on: false
     });
 
+	plug._init = function (guiBuilder) {
+
+        };
+
+    plug._applyTo = function (meshFile, on) {
+        if (on == false) {
+            scene.removeOverlayLayer(meshFile, plug.getName());
+            return;
+        }
+
+        var geom = meshFile.getThreeMesh().geometry.clone();
+
+        var bbox = new THREE.BoxHelper(meshFile.getThreeMesh());
+
+        scene.addOverlayLayer(meshFile, plug.getName(), bbox);
+    };
 
     plugin.Manager.install(plug);
 
