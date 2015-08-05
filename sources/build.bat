9b64f51df30d952d28d115f014c8b765f63d4794
gmdelogu <gmdelogu@gmail.com>
2015-08-05 20:14:07 +0200

    Removed the useless MeshLabJs.cpp file and updated vcglib path

--- sources/build.bat
+++ sources/build.bat
@@ -1,1 +1,1 @@
-emcc --bind -s DEMANGLE_SUPPORT=1 -I ../../vcglib/vcglib/ MeshLabJs.cpp CppMesh.cpp Meshing.cpp Sampling.cpp Create.cpp Refine.cpp Smooth.cpp Saver.cpp Random.cpp ../../vcglib/vcglib/wrap/ply/plylib.cpp  -s TOTAL_MEMORY=536870912 -O3 -o ../js/generated/MeshLabCppCore.js
+emcc --bind -s DEMANGLE_SUPPORT=1 -I ../../vcglib/ CppMesh.cpp Meshing.cpp Sampling.cpp Create.cpp Refine.cpp Smooth.cpp Saver.cpp Random.cpp ../../vcglib/wrap/ply/plylib.cpp  -s TOTAL_MEMORY=536870912 -O3 -o ../js/generated/MeshLabCppCore.js
