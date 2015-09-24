1a4ccbeb09d376719828e9c6be2e43845c96d23c
Paolo Cignoni <cignoni@isti.cnr.it>
2015-09-24 12:42:58 +0200

    Added Selection invert/none/all cpp code

--- sources/Refine.cpp
+++ sources/Refine.cpp
@@ -41,26 +41,51 @@ void RefineMesh(uintptr_t _baseM, int step, int alg)
   printf("Refined mesh %i vert - %i face \n",m.VN(),m.FN());
 }
 
-void DilateSelection(uintptr_t _baseM)
+void SelectionDilate(uintptr_t _baseM)
 {
   MyMesh &m = *((MyMesh*) _baseM);
   tri::UpdateSelection<MyMesh>::VertexFromFaceLoose(m);
   tri::UpdateSelection<MyMesh>::FaceFromVertexLoose(m);
 }
 
-void ErodeSelection(uintptr_t _baseM)
+void SelectionErode(uintptr_t _baseM)
 {
   MyMesh &m = *((MyMesh*) _baseM);
   tri::UpdateSelection<MyMesh>::VertexFromFaceStrict(m);
   tri::UpdateSelection<MyMesh>::FaceFromVertexStrict(m);
 }
 
+void SelectionInvert(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexInvert(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceInvert(m);
+}
+
+void SelectionAll(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexAll(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceAll(m);
+}
+
+void SelectionNone(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexClear(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceClear(m);
+}
+
+
 #ifdef __EMSCRIPTEN__
 //Binding code
 using namespace emscripten;
 EMSCRIPTEN_BINDINGS(MLRefinePlugin) {
   emscripten::function("RefineMesh", &RefineMesh);
-  emscripten::function("ErodeSelection", &ErodeSelection);
-  emscripten::function("DilateSelection", &DilateSelection);
+  emscripten::function("SelectionErode", &SelectionErode);
+  emscripten::function("SelectionDilate", &SelectionDilate);
+  emscripten::function("SelectionInvert", &SelectionInvert);
+  emscripten::function("SelectionAll",  &SelectionAll);
+  emscripten::function("SelectionNone", &SelectionNone);
 }
 #endif
