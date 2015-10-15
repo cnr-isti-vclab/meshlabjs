cedf0ccc34d2b7825863597d63674d08eb9178bb
Paolo Cignoni <cignoni@isti.cnr.it>
2015-10-15 13:07:20 +0200

    Small refactoring on cpp code
    Renamed files and moved functions

--- sources/Refine.cpp
+++ sources/Refine.cpp
@@ -41,40 +41,6 @@ void RefineMesh(uintptr_t _baseM, int step, int alg)
   printf("Refined mesh %i vert - %i face \n",m.VN(),m.FN());
 }
 
-void SelectionDilate(uintptr_t _baseM)
-{
-  MyMesh &m = *((MyMesh*) _baseM);
-  tri::UpdateSelection<MyMesh>::VertexFromFaceLoose(m);
-  tri::UpdateSelection<MyMesh>::FaceFromVertexLoose(m);
-}
-
-void SelectionErode(uintptr_t _baseM)
-{
-  MyMesh &m = *((MyMesh*) _baseM);
-  tri::UpdateSelection<MyMesh>::VertexFromFaceStrict(m);
-  tri::UpdateSelection<MyMesh>::FaceFromVertexStrict(m);
-}
-
-void SelectionInvert(uintptr_t _baseM, bool vertFlag, bool faceFlag)
-{
-  MyMesh &m = *((MyMesh*) _baseM);
-  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexInvert(m);
-  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceInvert(m);
-}
-
-void SelectionAll(uintptr_t _baseM, bool vertFlag, bool faceFlag)
-{
-  MyMesh &m = *((MyMesh*) _baseM);
-  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexAll(m);
-  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceAll(m);
-}
-
-void SelectionNone(uintptr_t _baseM, bool vertFlag, bool faceFlag)
-{
-  MyMesh &m = *((MyMesh*) _baseM);
-  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexClear(m);
-  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceClear(m);
-}
 
 
 #ifdef __EMSCRIPTEN__
--- sources/Selection.cpp
+++ sources/Selection.cpp
@@ -38,6 +38,41 @@ void RandomSelection(uintptr_t _m, float vertprob, float faceprob)
     }
 }
 
+void SelectionDilate(uintptr_t _baseM)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  tri::UpdateSelection<MyMesh>::VertexFromFaceLoose(m);
+  tri::UpdateSelection<MyMesh>::FaceFromVertexLoose(m);
+}
+
+void SelectionErode(uintptr_t _baseM)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  tri::UpdateSelection<MyMesh>::VertexFromFaceStrict(m);
+  tri::UpdateSelection<MyMesh>::FaceFromVertexStrict(m);
+}
+
+void SelectionInvert(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexInvert(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceInvert(m);
+}
+
+void SelectionAll(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexAll(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceAll(m);
+}
+
+void SelectionNone(uintptr_t _baseM, bool vertFlag, bool faceFlag)
+{
+  MyMesh &m = *((MyMesh*) _baseM);
+  if(vertFlag) tri::UpdateSelection<MyMesh>::VertexClear(m);
+  if(faceFlag) tri::UpdateSelection<MyMesh>::FaceClear(m);
+}
+
 
 #ifdef __EMSCRIPTEN__
 //Binding code
