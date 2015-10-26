975f614ed8a80fb03c437735fd89a98ce471b610
Paolo Cignoni <cignoni@isti.cnr.it>
2015-10-26 20:09:24 +0100

    Housekeeping of the location of the js filter plugins

--- js/mlj/plugins/filters/Meshing.js
+++ js/mlj/plugins/filters/Meshing.js
@@ -57,121 +57,7 @@
         scene.updateLayer(meshFile);
     };
     
-    var SelectionDilateFilter = new plugin.Filter({
-        name: "Selection Face Dilate",
-        tooltip: "Perform a morphological dilate operation on the set of the selected faces",
-        arity: 1
-    });
 
-    SelectionDilateFilter._init = function (builder) {  };
-
-    SelectionDilateFilter._applyTo = function (meshFile) {
-        Module.SelectionDilate(meshFile.ptrMesh());
-        scene.updateLayer(meshFile);
-    };
-    
-    var SelectionErodeFilter = new plugin.Filter({
-        name: "Selection Face Erode",
-        tooltip: "Perform a morphological erosion operation on the set of the selected faces",
-        arity: 1
-    });
-
-    SelectionErodeFilter._init = function (builder) {  };
-
-    SelectionErodeFilter._applyTo = function (meshFile) {
-        Module.SelectionErode(meshFile.ptrMesh());
-        scene.updateLayer(meshFile);
-    };
-    
-    var SelectionAllFilter = new plugin.Filter({
-        name: "Selection All",
-        tooltip: "Select all the element of the mesh. Can works on faces, vertex or both.",
-        arity: 1
-    });
-    var SelectionAllWidget;
-    
-    SelectionAllFilter._init = function (builder) { 
-    SelectionAllWidget = builder.Choice({
-            label: "Element",
-            tooltip: "Select to what element the filter should be applied",
-            options: [
-                {content: "Both", value: USE_BOTH, selected: true},
-                {content: "Face", value: USE_FACE},
-                {content: "Vert", value: USE_VERT}
-            ]
-        });
-    };
-
-    SelectionAllFilter._applyTo = function (meshFile) {
-        var vertFlag,faceFlag;
-        switch(SelectionAllWidget.getValue()) {
-            case USE_BOTH: vertFlag = true;  faceFlag = true; break;
-            case USE_FACE: vertFlag = false; faceFlag = false; break;
-            case USE_VERT: vertFlag = true;  faceFlag = false; break;
-        }
-        Module.SelectionAll(meshFile.ptrMesh(),vertFlag,faceFlag);
-        scene.updateLayer(meshFile);
-    };
-    
-    var SelectionNoneFilter = new plugin.Filter({
-        name: "Selection None",
-        tooltip: "Clear current selection.",
-        arity: 1
-    });
-    var SelectionNoneWidget;
-    SelectionNoneFilter._init = function (builder) {  
-    SelectionNoneWidget = builder.Choice({
-            label: "Element",
-            tooltip: "Select to what element the filter should be applied",
-            options: [
-                {content: "Both", value: USE_BOTH, selected: true},
-                {content: "Face", value: USE_FACE},
-                {content: "Vert", value: USE_VERT}
-            ]
-        });
-    };
-
-    SelectionNoneFilter._applyTo = function (meshFile) {
-        var vertFlag,faceFlag;
-        switch(SelectionAllWidget.getValue()) {
-            case USE_BOTH: vertFlag = true;  faceFlag = true; break;
-            case USE_FACE: vertFlag = false; faceFlag = false; break;
-            case USE_VERT: vertFlag = true;  faceFlag = false; break;
-        }
-        Module.SelectionNone(meshFile.ptrMesh(),vertFlag,faceFlag);
-        scene.updateLayer(meshFile);
-    };
-    
-    var SelectionInvertFilter = new plugin.Filter({
-        name: "Selection Invert",
-        tooltip: "Invert the current selection",
-        arity: 1
-    });
-
-    var SelectionInvertWidget;
-    SelectionInvertFilter._init = function (builder) { 
-    SelectionInvertWidget = builder.Choice({
-            label: "Element",
-            tooltip: "Select to what element the filter should be applied",
-            options: [
-                {content: "Both", value: USE_BOTH, selected: true},
-                {content: "Face", value: USE_FACE},
-                {content: "Vert", value: USE_VERT}
-            ]
-        });
-    };
-
-    SelectionInvertFilter._applyTo = function (meshFile) {
-        var vertFlag,faceFlag;
-        switch(SelectionAllWidget.getValue()) {
-            case USE_BOTH: vertFlag = true;  faceFlag = true; break;
-            case USE_FACE: vertFlag = false; faceFlag = false; break;
-            case USE_VERT: vertFlag = true;  faceFlag = false; break;
-        }
-        Module.SelectionInvert(meshFile.ptrMesh(),vertFlag,faceFlag);
-        scene.updateLayer(meshFile);
-    };
-    
     var ConvexHullFilter = new plugin.Filter({
         name: "Convex Hull",
         tooltip: "Create a new layer with the convex hull of the vertexes of the current mesh. "+
@@ -188,40 +74,9 @@
         scene.addLayer(newmeshFile);
     };
 
-     var SelectionByQualityFilter = new plugin.Filter({
-        name: "Selection by Quality",
-        tooltip: "Select in the current layer the vertex/face whose quality is below a given threshold.",
-        arity: 1
-    });
-    
-    var thresholdWidget,vertexFaceWidget;
-    SelectionByQualityFilter._init = function (builder) {
-         thresholdWidget = builder.RangedFloat({
-            max: 1.0, min: 0.0, step: 0.01, defval: 0.5,
-            label: "Threshold %",
-            tooltip: "As a percentage of the range of the quality"
-        });
-           vertexFaceWidget = builder.Bool({
-            defval: true,
-            label: "Per vertex",
-            tooltip: "If true, vertex quality is used and vertexes are selected, otherwise the filter work by face."
-        });
-    };
-
-    SelectionByQualityFilter._applyTo = function (basemeshFile) {
-        Module.SelectionByQuality(basemeshFile.ptrMesh(),thresholdWidget.getValue(),vertexFaceWidget.getValue());
-        scene.updateLayer(basemeshFile);
-    };
-    
     
     plugin.Manager.install(QuadricSimpFilter);
     plugin.Manager.install(ClusteringFilter);
-    plugin.Manager.install(SelectionDilateFilter);
-    plugin.Manager.install(SelectionErodeFilter);
-    plugin.Manager.install(SelectionAllFilter);
-    plugin.Manager.install(SelectionNoneFilter);
-    plugin.Manager.install(SelectionInvertFilter);
-    plugin.Manager.install(SelectionByQualityFilter);
     plugin.Manager.install(ConvexHullFilter);
     
 
--- js/mlj/plugins/filters/Smooth.js
+++ js/mlj/plugins/filters/Smooth.js
@@ -1,6 +1,6 @@
 
 (function (plugin, scene) {
-
+/******************************************************************************/
     var LaplacianSmoothFilter = new plugin.Filter({
         name: "Laplacian Smooth",
         tooltip: "Perform Geometric Laplacian Smoothing on the vertices of the mesh",
@@ -8,31 +8,24 @@
     });
 
     var stepLWidget, weightWidget;
-
     LaplacianSmoothFilter._init = function (builder) {
-
         stepLWidget = builder.Integer({
             min: 1, step: 1, defval: 1,
             label: "Iteration",
             tooltip: "Number of iteration of the smoothing algorithm"
         });
-
         weightWidget = builder.Bool({
             defval: false,
             label: "Cotangent Weights",
             tooltip: "Use cotangent weighting scheme during relaxation."
         });
-
     };
 
     LaplacianSmoothFilter._applyTo = function (meshFile) {
         Module.LaplacianSmooth(meshFile.ptrMesh(), stepLWidget.getValue(), weightWidget.getValue());
         scene.updateLayer(meshFile);
     };
-
-    plugin.Manager.install(LaplacianSmoothFilter);
-
-
+/******************************************************************************/
     var TaubinSmoothFilter = new plugin.Filter({
         name: "Taubin Smooth",
         tooltip: "The &lambda;-&mu; Taubin smoothing, it make two steps of smoothing, forth and back, for each iteration. Based on:<br>" +
@@ -42,35 +35,61 @@
     });
 
     var stepTWidget, lambdaWidget, muWidget;
-
     TaubinSmoothFilter._init = function (builder) {
-
         stepTWidget = builder.Integer({
             min: 1, step: 1, defval: 1,
             label: "Iteration",
             tooltip: "Number of iteration of the smoothing algorithm"
         });
-
         lambdaWidget = builder.Float({
             max: 1, min: 0.0, step: 0.1, defval: 0.330,
             label: "lambda",
             tooltip: "The lambda parameter of the Taubin Smoothing algorithm"
         });
-
         muWidget = builder.Float({
             max: 0.0, min: -1.0, step: 0.1, defval: -0.34,
             label: "mu",
             tooltip: "The mu parameter of the Taubin Smoothing algorithm"
         });
-
     };
 
     TaubinSmoothFilter._applyTo = function (meshFile) {
         Module.TaubinSmooth(meshFile.ptrMesh(), stepTWidget.getValue(), lambdaWidget.getValue(), muWidget.getValue());
         scene.updateLayer(meshFile);
     };
+/******************************************************************************/
+    var RndDisplacementFilter = new plugin.Filter({
+            name:"Random Displacement",
+            tooltip:null,
+            arity:1
+        });
 
+    var displWidget, normalDispWidget;
+    RndDisplacementFilter._init = function (builder) {
+
+        displWidget = builder.Float({
+            max: 0.1, min: 0.01, step: 0.01, defval: 0.01,
+            label: "Displacement",
+            tooltip: "Amount of random displacement added to each vertex of the mesh"
+        });
+           normalDispWidget = builder.Bool({
+            defval: false,
+            label: "Normal Directed Displ.",
+            tooltip: "If true, the displacement is directed along the normal of the surface, otherwise it is in a random direction."
+        });
+
+    };
+
+    RndDisplacementFilter._applyTo = function (meshFile) {
+        Module.RandomDisplacement(meshFile.ptrMesh(), displWidget.getValue(),normalDispWidget.getValue());
+        scene.updateLayer(meshFile);
+    };
+
+/******************************************************************************/
+
+    plugin.Manager.install(LaplacianSmoothFilter);
     plugin.Manager.install(TaubinSmoothFilter);
+    plugin.Manager.install(RndDisplacementFilter);
 
 
 })(MLJ.core.plugin, MLJ.core.Scene);
