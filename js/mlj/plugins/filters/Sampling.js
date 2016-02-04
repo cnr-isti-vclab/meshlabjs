f575d050cfe74a232526a04852b503046447f63d
Paolo Cignoni <cignoni@isti.cnr.it>
2016-02-04 19:19:57 +0100

    Added VoronoiScaffoldingFilter
    Porting of the old meshlab filter for building voronoi scaffolding

--- js/mlj/plugins/filters/Sampling.js
+++ js/mlj/plugins/filters/Sampling.js
@@ -1,5 +1,9 @@
 
+/* global MLJ, Module */
+
 (function (plugin, scene) {
+
+/******************************************************************************/  
 
     var MontecarloSamplingFilter = new plugin.Filter({
         name: "Montecarlo Random Sampling",
@@ -33,8 +37,7 @@
         scene.addLayer(newLayer);
     };
 
-    plugin.Manager.install(MontecarloSamplingFilter);
-
+/******************************************************************************/  
 
     var PoissonDiskSamplingFilter = new plugin.Filter({
         name: "Poisson Disk Sampling",
@@ -70,11 +73,10 @@
                                    radiusWidget.getValue(),sampleNumPDWidget.getValue(),  0);
         scene.addLayer(newLayer);
     };
+    
+/******************************************************************************/  
 
-    plugin.Manager.install(PoissonDiskSamplingFilter);
-
-
-var VolumeMontecarloSamplingFilter = new plugin.Filter({
+    var VolumeMontecarloSamplingFilter = new plugin.Filter({
         name: "Volume Montecarlo Sampling",
         tooltip: "Compute a volumetric montecarlo sampling inside a watertight mesh.",
         arity: 2
@@ -105,11 +107,10 @@ var VolumeMontecarloSamplingFilter = new plugin.Filter({
             scene.addLayer(newLayer);
         }
     };
+    
+/******************************************************************************/  
 
-    plugin.Manager.install(VolumeMontecarloSamplingFilter);
-    
-    
-var VolumePoissonSamplingFilter = new plugin.Filter({
+    var VolumePoissonSamplingFilter = new plugin.Filter({
         name: "Volume Poisson Sampling",
         tooltip: "Compute a volumetric Poisson sampling inside a watertight mesh.",
         arity: 2
@@ -141,5 +142,105 @@ var VolumePoissonSamplingFilter = new plugin.Filter({
         }
     };
 
+/******************************************************************************/  
+
+    var VoronoiScaffoldingFilter = new plugin.Filter({
+        name: "Voronoi Scaffolding",
+        tooltip: "Build a mesh representing the various elements of a Voronoi"
+                +"tessellation of the interior of a given mesh.<br>"
+                +"A number of seeds is distributed in the volume using a Poisson Disk"
+                +"distribution and then relaxed using a barycentric Lloyd relaxation strategy."
+                +"From these seeds we generate an implicit volumetric representation of the voronoi diagram" 
+                +"and then an isosurface is extracted.",
+        arity: 2
+    });
+
+    var VSradiusWidget,VSincludeSurfaceFlag,VSscaffoldingTypeWidget,VSVoxelPercWdg,
+    VSthicknessWidget,VSRelaxStepWdg,VSRandomSeedWdg;
+     
+    VoronoiScaffoldingFilter._init = function (builder) {
+
+        VSradiusWidget = builder.Float({
+            min: 0.0, step: 0.01, defval: "0.05",
+            label: "Poisson Radius",
+            tooltip: "The poisson sphere radius. Expressed as a percentage of the bbox diagonal."
+                    +"It is the main parameter that regulates the number of voronoi seeds."
+        });
+        
+        VSincludeSurfaceFlag = builder.Bool({
+            defval: false,
+            label: "Include Original Surface",
+            tooltip: "If true, the original surface is included in the implicit reprsentation. "
+        });
+        
+        VSscaffoldingTypeWidget  = builder.Choice({
+            label: "Scaffolding type",
+            tooltip: "Choose the element of the voronoi diagram that is ",
+            options: [
+                {content: "Seed", value: "0"},
+                {content: "Edge", value: "1", selected: true},
+                {content: "Face", value: "2"},
+                {content: "Corner", value: "3"}
+            ]
+        });
+        
+        VSthicknessWidget = builder.Float({
+            min: 0.0, step: 0.001, defval: "0.01",
+            label: "Thickness",
+            tooltip: "The thickness of the generated structure. <br>"
+                    +"Expressed as a percentage of the bbox diagonal. "
+                    +"It should be greater or equal than the voxel size"
+        });
+        
+        VSVoxelPercWdg = builder.Float({
+            min: 0.0, step: 0.001, defval: "0.005",
+            label: "Voxel size",
+            tooltip: "Expressed as a percentage of the bbox diagonal, it represents the side of the voxel of the volumetric represetation.<br>"
+                    +"Lowering this value increase the precision of the computed structure at expense of computational time. <br>"
+                    +"Be careful, halving this value means more or less a x8 increase in computation time."
+        });
+        
+        VSRelaxStepWdg = builder.Integer({
+            max: 10, min: 1, step: 1, defval: 1,
+            label: "Step",
+            tooltip: "How many lloyd relaxation are performed to the seeds. <br>"
+                    +"Higher values give more uniform seed distribution"
+        });
+        
+        VSRandomSeedWdg = builder.Integer({
+            step: 1, defval: 1,
+            label: "Random Seed",
+            tooltip: "The random seed used in generation. <br>"
+                    +"Set it to a nonzero value for repeatability."
+                    +"If zero is intialized with current clock time."
+        });
+
+    };
+
+    VoronoiScaffoldingFilter._applyTo = function (baseLayer) {
+
+        var newLayer = MLJ.core.Scene.createLayer("Voronoi Scaffolding");
+        newLayer.cppMesh.addPerVertexNormal();
+        var ret = Module.CreateVoronoiScaffolding(baseLayer.ptrMesh(), newLayer.ptrMesh(),
+                                   VSradiusWidget.getValue(),VSRelaxStepWdg.getValue(),
+                                   parseInt(VSscaffoldingTypeWidget.getValue()),
+                                   VSVoxelPercWdg.getValue(),VSthicknessWidget.getValue(),
+                                   VSincludeSurfaceFlag.getValue(),VSRandomSeedWdg.getValue());
+                                   
+        if(ret===true)
+        {            
+            newLayer.cppMesh.addPerVertexColor();
+            //newLayer.overlaysParams.getByKey("ColorWheel").mljColorMode = MLJ.ColorMode.Vertex;
+            scene.addLayer(newLayer);
+        }
+    };
+
+/******************************************************************************/  
+ 
+    plugin.Manager.install(MontecarloSamplingFilter);
+    plugin.Manager.install(PoissonDiskSamplingFilter);
+    plugin.Manager.install(VolumeMontecarloSamplingFilter);
     plugin.Manager.install(VolumePoissonSamplingFilter);
+    plugin.Manager.install(VoronoiScaffoldingFilter);
+    
 })(MLJ.core.plugin, MLJ.core.Scene);
