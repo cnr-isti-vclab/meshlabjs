71e972d9da7b3e8ad7eecc5bcb9f3ee094a17f7e
Paolo Cignoni <cignoni@isti.cnr.it>
2016-02-18 00:25:52 +0100

    Added option in the quadric simplification to preserve topology

--- js/mlj/plugins/filters/Meshing.js
+++ js/mlj/plugins/filters/Meshing.js
@@ -10,23 +10,29 @@
         arity: 1
     });
 
-    var ratioW, qualityQuadricW;
+    var ratioWdg, qualityQuadricWdg, topologicalQuadricWdg;
     QuadricSimpFilter._init = function (builder) {
-        ratioW = builder.RangedFloat({
+        ratioWdg = builder.RangedFloat({
             max: 1, min: 0, step: 0.1, defval: 0.5,
             label: "Simplification Ratio",
             tooltip: "Amount of Simplification expressed as a percentage of the initial mesh complexity."
         });
-    qualityQuadricW = builder.Bool({
+    qualityQuadricWdg = builder.Bool({
             defval: true,
             label: "Quality Quadric",
             tooltip: "if true during the initialization it manages all the edges as border edges by adding a set of additional quadrics that are useful mostly for keeping face aspect ratio good."
+        });
+    topologicalQuadricWdg = builder.Bool({
+            defval: true,
+            label: "Toplogical Preservarion",
+            tooltip: "if true during the simplification the topology of the mesh is preserved. This avoid the creation of small non manifold but also prevent drastic simplification ratios."
         });
 
     };
 
     QuadricSimpFilter._applyTo = function (meshFile) {
-        Module.QuadricSimplification(meshFile.ptrMesh(), ratioW.getValue(), 0, qualityQuadricW.getValue());
+        Module.QuadricSimplification(meshFile.ptrMesh(), ratioWdg.getValue(), 0, 
+                                     topologicalQuadricWdg.getValue(), qualityQuadricWdg.getValue());
     };
 /******************************************************************************/  
     var ClusteringFilter = new plugin.Filter({
