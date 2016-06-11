417fd8a69c74dc9ef092686831719de81b36f22b
T.Alderighi <thomasalderighi@gmail.com>
2016-06-11 14:47:11 +0200

    chasing a bug in SM.js render function

--- js/mlj/plugins/rendering/shaders/ShadowFrag.glsl
+++ js/mlj/plugins/rendering/shaders/ShadowFrag.glsl
@@ -8,9 +8,10 @@ varying vec4 lightFragPos;
 
 void main(){
 
+
   float eyeClosest = texture2D(eyeDepthMap, gl_FragCoord.xy).r;
 
-// if (gl_FragCoord.z > eyeClosest ) discard;
+//  if (gl_FragCoord.z > eyeClosest ) discard;
 
   vec4 position = lightFragPos;
   position.xyz /= position.w;
@@ -19,8 +20,10 @@ void main(){
   float closest = texture2D(lightDepthMap, position.xy).r;
   float current = position.z;
 
-  //gl_FragColor.xyzw = vec4(vec3(eyeClosest), 1.0);
-  //return;
+// DEBUG...USE IT TO LOOK AT lightDepthBuffer or cameraDepthBuffer
+  gl_FragColor.xyzw = vec4(vec3(closest), 1.0);
+  return;
+
   if (current - 0.005 > closest){
     gl_FragColor = vec4(0, 0, 0, 0.3);
   } else {
