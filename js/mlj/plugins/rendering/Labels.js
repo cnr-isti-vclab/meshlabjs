3bc023d6736d108241dbe22674d1c97832036cd8
gmdelogu <gmdelogu@gmail.com>
2016-07-24 17:51:07 +0200

    -Handling window resize -Performance improv.

--- js/mlj/plugins/rendering/Labels.js
+++ js/mlj/plugins/rendering/Labels.js
@@ -19,6 +19,9 @@
 
     var labels = [];
     var updateLabels = false;
+    var my3D = null;
+    var myCanvas = null;
+    var myBody = null;
 
     plug._init = function (guiBuilder) {
 
@@ -91,7 +94,10 @@
         if (on === false) {
             $('.label').remove();
             labels = [];    
-            $("canvas").unbind("mousedown mousemove", updateLabels);
+            myCanvas.unbind("mousemove mousewheel", updateLabels);
+            myCanvas.unbind("mousedown", startUpdating);
+            myCanvas.unbind("mouseup", stopUpdating);
+            $(window).unbind("resize", updateLabels);
             return;
         }
 
@@ -99,43 +105,50 @@
 
         //Getting the mesh vertices
         var vv = meshFile.threeMesh.geometry.getAttribute('position');
+
+        my3D = $('#_3D');
+        myCanvas = $('canvas');
+        myBody = document.getElementsByTagName('body')[0];
         
         //Creating label, one for vertex        
         for(var ii = 0; ii < vv.length; ii+=3)
         {
             var pos = toScreenPosition(vv.array[ii],vv.array[ii+1],vv.array[ii+2]);
-            labels.push(addLabel(Math.round(ii/3) + "", 'body', $('#_3D').position().left + pos.x, pos.y));
+            labels.push(addLabel(Math.round(ii/3) + "", my3D.position().left + pos.x, pos.y));
+            
         }
 
         updateLabels();
         
         //Binding the update function to the mouse events
-        $('canvas').bind("mousemove mousewheel", updateLabels);
-        $("canvas").bind("mousedown", startUpdating);
-        $('canvas').bind("mouseup", stopUpdating);
-
+        myCanvas.bind("mousewheel", updateLabels);
+        myCanvas.bind("mousedown", startUpdating);
+        myCanvas.bind("mouseup", stopUpdating);
+        $(window).bind("resize", updateLabels);
+        
         function startUpdating()
         {
-            $('canvas').bind("mousemove", updateLabels);
+            myCanvas.bind("mousemove", updateLabels);
         }
 
         function stopUpdating()
         {
-            $("canvas").unbind("mousemove", updateLabels);
+            myCanvas.unbind("mousemove", updateLabels);
         }
 
         function updateLabels()
         {
-            for(var ii = 0; labels.length > 0 && ii < vv.length; ii+=3)
+            if(labels.length <= 0)
+                return;
+
+            for(var ii = 0; ii < vv.length; ii+=3)
             {
                 var pos = toScreenPosition(vv.array[ii],vv.array[ii+1],vv.array[ii+2]);
                 var lIndex = Math.round(ii/3);
                 labels[lIndex].style.top = pos.y + 'px';
-                labels[lIndex].style.left = $('#_3D').position().left + pos.x + 'px';
+                labels[lIndex].style.left = my3D.position().left + pos.x + 'px';
 
-
-                var canvas = $('canvas');
-                if(pos.x < 0 || pos.x > canvas.attr('width') || pos.y < 0 || pos.y > canvas.attr('height'))
+                if(pos.x < 0 || pos.x > myCanvas.attr('width') || pos.y < 0 || pos.y > myCanvas.attr('height'))
                     labels[lIndex].style.visibility = 'hidden';
                 else
                     labels[lIndex].style.visibility = 'visible';
@@ -147,7 +160,6 @@
         {
 
             var vector = new THREE.Vector3(x, y, z);
-            var canvas = $('canvas');
             var camera = MLJ.core.Scene.getCamera();
             camera.updateProjectionMatrix();
 
@@ -155,8 +167,8 @@
             vector.project(camera);
 
             // map to 2D screen space
-            vector.x = Math.round( (   vector.x + 1 ) * canvas.attr('width')  / 2 );
-            vector.y = Math.round( ( - vector.y + 1 ) * canvas.attr('height') / 2 );
+            vector.x = Math.round( (   vector.x + 1 ) * myCanvas.attr('width')  / 2 );
+            vector.y = Math.round( ( - vector.y + 1 ) * myCanvas.attr('height') / 2 );
             vector.z = 0;
                                             
             return { 
@@ -166,7 +178,7 @@
 
         }
 
-        function addLabel(txt, target, left, top)
+        function addLabel(txt, left, top)
         {
             var text = document.createElement('div');
             
@@ -184,7 +196,7 @@
             text.style.top = top + 'px';
             text.style.left = left + 'px';
 
-            $(target).append(text);            
+            myBody.appendChild(text);            
 
             return text;
         }
